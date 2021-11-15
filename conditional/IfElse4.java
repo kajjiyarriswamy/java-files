5d859cdd45ea1a4d7634e1ef94a29853b5f25709
yarriswamy <yarriswamy536@outlook.com>
2021-11-15 14:49:07 +0530

    fouth change

--- conditional/IfElse4.java
+++ conditional/IfElse4.java
@@ -1,4 +1,4 @@
-//write a program to check the whether the given character is lowercase done .
+//write a program to check the whether the given character is lowercase done3 .
 class IfElse4{
  public static void main(String[] args){
     char j='a';
