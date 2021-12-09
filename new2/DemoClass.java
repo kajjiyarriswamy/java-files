803c389e65c7b343a3eb37cc29986a4c5fe6fc37
yarriswamy <yarriswamy536@outlook.com>
2021-12-09 13:46:28 +0530

    DateAndTime features

---
+++ new2/DemoClass.java
@@ -0,0 +1,16 @@
+package com.technoelevate.date_and_time.programs;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+LocalTime t = LocalTime.now();
+System.out.println(t);
+LocalDate d = LocalDate.now();
+System.out.println(d);
+
+	}
+
+}
